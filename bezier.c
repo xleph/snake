36d68685135ad28243f4772fe173df13ac785943
mllegoman <pixel.x.widow@gmail.com>
2021-10-04 00:28:13 -0400

    fixed origin gravitational bug

--- bezier.c
+++ bezier.c
@@ -24,49 +24,48 @@ return ((n>>i)+((n>>i)&1))<<i;
 }
 
 void drawcurve(unsigned long rgb, uint *x, uint *y, uchar n, int sec, int size, char win) {
-uchar i, j;
-uint slice[n + 1];
-float t, tc, snbl, totx, toty;
+float xslice[n + 1];
+float yslice[n + 1];
+float t, tc, totx, toty;
 tc = (float)1.0/adjpow(size);
 int xc;
 int yc;
-snbl = 1;
-
-slice[0] = 1;
-	for (i = 1; i < (n + 1)/2; i++) {
-	slice[i] = slice[i - 1] * (n - i)/i;
-	}
-
-	for (i = n - 1; i > (n - 1)/2; i--) {
-	slice[i] = slice[n - i - 1];
-	}
 
 	for (t = 0; (uint)t==0; t+=tc+tc*(t>0.25||t>0.75)) {
-		totx=0;
-		toty=0;
-		i  = 0;
-		while (i < n) { // n-1 times (for each term)
-		snbl = 1;
-			for (j = 0; j < n - i - 1; j++) {
-			snbl*=(1 - t);
-			}
-			for (j = 0; j < i; j++) {
-			snbl*=t;
-			}
-
-		snbl*=slice[i];
-
-		totx+=x[i]*snbl;
-		toty+=y[i]*snbl;
-		i++;
+	xslice[0] = 1;
+	yslice[0] = 1;
+	xc = 1;
+		while (xc < n) {
+		xslice[xc] = t * xslice[xc - 1] * (n - xc) / xc;
+		yslice[xc] = t * yslice[xc - 1] * (n - xc) / xc;
+		xc++;
 		}
-		
+	xc = 0;
+		while (xc < n) {
+		xslice[xc]*=x[xc];
+		yslice[xc]*=y[xc];
+		xc++;
+		}
+	totx=xslice[n - 1];
+	toty=yslice[n - 1];
+	xc = 0;
+		while (xc < n - 1) {
+			for (yc = 0; yc < n - xc - 1; yc++) {
+			xslice[xc]*=(1 - t);
+			yslice[xc]*=(1 - t);
+			}
+		totx+=xslice[xc];
+		toty+=yslice[xc];
+		xc++;
+		}
+
 		xc = -1*sec/2;
-		while (xc < sec/2) {
+		while (xc < sec/2 + 1) {
 		yc = (sec*sec/4-xc*xc)*FISR(sec*sec/4-xc*xc);
 		RegionFill((uint)totx+xc, (uint)toty+yc, 1, 1, rgb, win);
 		RegionFill((uint)totx+xc, (uint)toty-yc, 1, 1, rgb, win);
 		xc++;
 		}
+	RegionFill((uint)totx+xc, (uint)toty, 1, 1, rgb, win);
 	}
 }
